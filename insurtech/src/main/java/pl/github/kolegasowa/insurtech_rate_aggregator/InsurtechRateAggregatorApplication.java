f857aa1f5cec6484f21d680c7d965115e22738ff
Pawel Zientara <pawelzientara1@wp.pl>
2026-03-20 21:00:20 +0100

    initialize project structure with Spring Boot and PostgreSQL setup

---
+++ insurtech/src/main/java/pl/github/kolegasowa/insurtech_rate_aggregator/InsurtechRateAggregatorApplication.java
@@ -0,0 +1,13 @@
+package pl.github.kolegasowa.insurtech_rate_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurtechRateAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurtechRateAggregatorApplication.class, args);
+	}
+
+}
---
+++ insurtech/src/test/java/pl/github/kolegasowa/insurtech_rate_aggregator/InsurtechRateAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.github.kolegasowa.insurtech_rate_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurtechRateAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
